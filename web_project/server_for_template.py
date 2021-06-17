a7f05d2ffdcb8b971bde8889d33566a53452979f
Bobrov Georgij <bobrov.georgij@yandex.ru>
2021-06-17 23:39:26 +0300

    Site modified Login and password added

--- web_project/server_for_template.py
+++ web_project/server_for_template.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 my_flask_app = Flask(__name__)
 
 @my_flask_app.route('/')
 def index():
 	return render_template('index.html')
+
+@my_flask_app.route('/all')
+def all():
+	return render_template('all.html')	
+
+@my_flask_app.route('/login/', methods=['POST'])
+def login():
+	return render_template('login.html', login=request.form.get('Admin'), password=request.form.get('00000000'))
 
 if __name__=='__main__':
 	my_flask_app.run(debug=True)
